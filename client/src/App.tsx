315a74d2a18ec88f92522dc29eac56c6784625a5
aldalistor <1.09465043e+08+aldalistor@users.noreply.github.com>
2026-09-13 16:29:09 +0000

    Add system setup tab and preserve Workbench windows

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,9 +6,10 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import WindowWorkbench from "./pages/WindowWorkbench";
+import SystemSetup from "./pages/SystemSetup";
 
 function Router() {
-  return <Switch><Route path="/" component={Home} /><Route path="/workbench" component={WindowWorkbench} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
+  return <Switch><Route path="/" component={WindowWorkbench} /><Route path="/home" component={Home} /><Route path="/workbench" component={WindowWorkbench} /><Route path="/setup" component={SystemSetup} /><Route path="/404" component={NotFound} /><Route component={NotFound} /></Switch>;
 }
 
 export default function App() {
